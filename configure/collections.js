bc58508536acb75766435ff37bb61ad1571451f1
Nandagopal-V <v.nandagopal18@gmail.com>
2022-08-26 12:11:55 +0530

    second commit

--- configure/collections.js
+++ configure/collections.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+let acSID=process.env.accountSID
+let aToken=process.env.authToken
 module.exports={
 
     PRODUCT_COLLECTION:'product1',
@@ -13,8 +16,8 @@ module.exports={
     BANNER_COLLECTION:'banner',
 
     serviceID:'VA962dee21c5d3e75fdca57868742a668a',
-    accountSID:'ACb98ab8d35c3dbd5b30d21344394483f3',
-    authToken:'8e4313ea7c319f595a9234f24c558105'
+    accountSID:acSID,
+    authToken:aToken
 
 
 }
